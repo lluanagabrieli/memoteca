5c9300f3da16a7de516d2f5c0dc8102eaacb81cd
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-06-12 19:14:41 -0300

    Adicionando novo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    CabecalhoComponent,
   ],
   imports: [
     BrowserModule,
