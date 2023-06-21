371e0ad4ce5482222336fc83aa0e66cf24c82321
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-06-20 22:09:14 -0300

    criando rotas para a aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+//representa as rotas da aplicação
+//path: caminho. Ele é em formato de string.
+//componente: o componente que vai ser utilizado nessa rota
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'listarPensamento',
+        pathMatch: 'full'
+        //quando o path está vazio, é preciso adicionar o pathMatch
+        //full: considera toda a URL
+        //prefix: considera só o endereço da página inicial - antes da primeira /
+    },
+    {
+        path: 'criarPensamento',
+        component: CriarPensamentoComponent
+    },
+
+    {
+        path: 'listarPensamento',
+        component: ListarPensamentoComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
