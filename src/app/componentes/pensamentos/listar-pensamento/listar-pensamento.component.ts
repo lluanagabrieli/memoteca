bd8f86445fa7c3366b4106597c9b2c89c7ad5a0b
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-06-27 08:27:53 -0300

    lista de pensamentos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
     listaPensamentos = [
-        /*{
+        {
             conteudo: 'Passo informações para o componente filho',
             autoria: 'Componente pai',
             modelo: 'modelo1'
@@ -16,6 +16,11 @@ export class ListarPensamentoComponent {
             conteudo: 'Minha propriedade é decorada com o @Input()',
             autoria: 'Componente filho',
             modelo: 'modelo2'
-        }*/
+        },
+        {
+            conteudo: 'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
+            autoria: 'testando o [ngClass]',
+            modelo: 'modelo1'
+        },
     ];
 }
