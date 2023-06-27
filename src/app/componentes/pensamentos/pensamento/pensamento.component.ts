a3add58dbc53c01b3d57b4870e9d0b0298930c49
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-06-27 08:25:02 -0300

    adição de uma condicional para pensamento

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -9,8 +9,17 @@ export class PensamentoComponent {
     //o decorator @Input() informa que irá receber informações do pai
     //dumb component: ele não pensa, somente recebe informações de fora. Por possuir esta característica ele também pode ser reutilizado.
     @Input() pensamento = {
-        conteudo: 'I love Angular',
-        autoria: 'Luana',
-        modelo: 'modelo2'
+        conteudo: '',
+        autoria: '',
+        modelo: ''
+    }
+
+    //o ngClass é um uma diretiva de atributos que adiciona classes CSS ao elemento
+    //ngFor e ngIf adicionam e removem elementos do template, sendo classificados como estruturais
+    larguraPensamento(): string{
+        if(this.pensamento.conteudo.length >= 256) {
+            return 'pensamento-g'
+        }
+        return 'pensamento-p'
     }
 }
