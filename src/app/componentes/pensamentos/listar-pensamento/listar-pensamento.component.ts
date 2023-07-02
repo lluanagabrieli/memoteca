10e3dfec766c4f416badb438f71896b2738e890c
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-07-02 13:21:37 -0300

    adicionando o serviço de listagem

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento/pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css']
 })
-export class ListarPensamentoComponent {
-    listaPensamentos:Pensamento[] = [
-    ];
-}
+
+export class ListarPensamentoComponent implements OnInit {
+
+    listaPensamentos: Pensamento[] = [];
+
+    constructor(private service: PensamentoService) { }
+
+    ngOnInit(): void {
+      this.service.listar().subscribe((listaPensamentos) => {
+        this.listaPensamentos = listaPensamentos
+      })
+    }
+  }
+
