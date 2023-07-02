a62fd25dd8095406a568f4afcc03f99844eab2bb
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-07-02 13:19:44 -0300

    adição de interface para comuncação com o backend

--- src/app/componentes/pensamentos/pensamento/pensamento.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.ts
@@ -1,6 +1,8 @@
 //A interface ou tipagem é como um contrato que deve ser seguido por ambas as partes — front-end e back-end.
+
+//adicionando o '?' na frente da variável, ela se torna opcional
 export interface Pensamento {
-    id: number
+    id?: number
     conteudo: string
     autoria: string
     modelo: string
