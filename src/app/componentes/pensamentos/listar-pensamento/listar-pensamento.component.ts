b1d6263f983de96a6d0f03a9c3cf16be472bfed9
Luana <luanagabrieli4599@hotmail.com>
2023-10-28 08:41:55 -0300

    :sparkles: : implementa botão para carregar mais pensamentos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,6 +12,7 @@ export class ListarPensamentoComponent implements OnInit {
 
     listaPensamentos: Pensamento[] = [];
     paginaAtual: number = 1;
+    haMaisPensamentos: boolean = true;
 
     constructor(private service: PensamentoService) { }
 
@@ -19,6 +20,17 @@ export class ListarPensamentoComponent implements OnInit {
       this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
         this.listaPensamentos = listaPensamentos
       })
+    }
+
+    carregarMaisPensamentos(){
+        this.service.listar(++this.paginaAtual)
+            .subscribe(listaPensamentos => {
+                this.listaPensamentos.push(...listaPensamentos);
+
+                if(!listaPensamentos.length){
+                    this.haMaisPensamentos = false;
+                }
+            })
     }
   }
 
